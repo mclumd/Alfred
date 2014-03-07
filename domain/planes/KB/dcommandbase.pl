18d1deaf1822abc7a57b020ecdc1bb7904001925
Emily Hand <e.hand@live.com>
2014-03-07 09:50:57 -0500

    added KB to planes domain

---
+++ domain/planes/KB/dcommandbase.pl
@@ -0,0 +1,46 @@
+isa(dcommand, 'fly').
+
+structure('fly', 
+	[[v0,v1,'O'], [v0,v2,'MV'], [v2,v3,'J']], 
+	[[v0,verb], [v1, plane], [v2, end], [v3, city]],
+	[v0,v1,v3]).
+
+structure('fly', 
+	[[v0,v1,'O'], [v0,v2,'MV'], [v2,v3,'J'], [v0,v4,'MV'], [v4,v5,'J']], 
+	[[v0,verb], [v1, plane], [v2, start], [v3, city], [v4, end], [v5, city]],
+	[v0,v1,v5]).
+
+structure('fly', 
+	[[v0,v2,'O'], [v1,v2,'A']], 
+	[[v0,verb], [v1, plane], [v2, city]],
+	[v0,v1,v2]).
+
+structure('fly', 
+	[[v0,v4,'0'], [v0,v4,'MV'], [v1,v3,'D'], [v2,v3,'AN'], [v4,v5,'J']],
+	[[v0,verb], [v2,v3,plane], [v4, end], [v5, city]],
+	[v0,v2,v3,v5]).
+
+isa(dcommand, 'load').
+
+structure('load', 
+	[[v0,v1,'O'], [v0,v2,'MV'], [v2,v3,'J']], 
+	[[v0,verb], [v1, parcel], [v2, end], [v3, plane]],
+	[v0,v1,v3]).
+
+structure('load', 
+	[[v0,v4,'0'], [v0,v4,'MV'], [v1,v3,'D'], [v2,v3,'AN'], [v4,v5,'J']],
+	[[v0,verb], [v2,v3,parcel], [v4, end], [v5, plane]],
+	[v0,v2,v3,v5]).
+
+isa(dcommand, 'find').
+
+structure('find',
+	  [[v0,v1,'O']],
+	  [[v0,verb], [v1, plane]],
+	  [v0,v1]).
+
+isa(observation, 'location').
+
+syntax('location', [v0, plane, city]).
+
+result(find, location).
---
+++ domain/planes/KB/dd.pl
@@ -0,0 +1,20 @@
+isa(domain, 'planes').
+isa(plane, 'Metroliner').
+isa(plane, 'Bullet').
+isa(plane, 'Northstar').
+isa(city, 'Buffalo').
+isa(city, 'Baltimore').
+isa(city, 'Richmond').
+isa(city, 'Pittsburgh').
+isa(city, 'Washington').
+isa(city, 'Newark').
+isa(city, 'Atlanta').
+isa(parcel, 'Package1').
+isa(parcel, 'Package2').
+isa(start, 'from').
+isa(end, 'to').
+
+origin('Metroliner','Buffalo').
+origin('Bullet', 'Baltimore').
+origin('Northstar', 'Richmond').
+    
